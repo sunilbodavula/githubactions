0387a2a5221a2c65dae2053b6a48412d488b3885
Sampathkumar012 <asampathkumar545@gmail.com>
2025-07-18 12:39:41 +0530

    Addeding on more build workflow

--- src/main/java/com/neo/githubaction/GithubactionApplication.java
+++ src/main/java/com/neo/githubaction/GithubactionApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    GithubactionApplication {
+public class   GithubactionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubactionApplication.class, args);
